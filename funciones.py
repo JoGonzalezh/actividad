2662bc8e5306023f56039a13205349fd6f26ebf7
NetAcad DEVASC <changeme@example.com>
2025-04-04 16:02:02 +0000

    Resta

--- funciones.py
+++ funciones.py
@@ -1,5 +1,7 @@
 def suma (a,b): 
     print (a+b)
+def resta (a,b):
+    print (a-b)
 
 
 
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #Menu Principal
-from funciones import suma
+from funciones import suma,resta
 while True:
     """
     1)suma  
-    2)
+    2)resta
     3)
     4)
     5) Salir
@@ -13,3 +13,7 @@ while True:
         num1 = int(input("Ingrese el primer numero "))
         num2 = int(input("Ingrese EL segundo numero "))
         suma (num1, num2)
+    elif opcion == 2:
+        num1 = int(input("Ingrese el primer numero "))
+        num2 = int(input("Ingrese EL segundo numero "))
+        resta (num1, num2)
