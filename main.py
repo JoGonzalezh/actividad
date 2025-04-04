e4548dc16591ef919894ba31ae8e72b11c4d97ac
NetAcad DEVASC <changeme@example.com>
2025-04-04 16:08:41 +0000

    multi y div

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 #Menu Principal
-from funciones import suma,resta
+from funciones import suma,resta,multi,div
 while True:
     """
-    1)suma  
-    2)resta
-    3)
-    4)
-    5) Salir
+    1)Suma  
+    2)Resta
+    3)Multiplicacion
+    4)Divicion
+    5)Salir
     """
     opcion = int(input("Seleccione su opcion: "))
     if opcion == 1:
         num1 = int(input("Ingrese el primer numero "))
         num2 = int(input("Ingrese EL segundo numero "))
         suma (num1, num2)
+
     elif opcion == 2:
         num1 = int(input("Ingrese el primer numero "))
         num2 = int(input("Ingrese EL segundo numero "))
         resta (num1, num2)
+
+    elif opcion == 3:
+        num1 = int(input("Ingrese el primer numero "))
+        num2 = int(input("Ingrese EL segundo numero "))
+        multi (num1, num2)
+
+    elif opcion == 4:
+        num1 = int(input("Ingrese el primer numero "))
+        num2 = int(input("Ingrese EL segundo numero "))
+        div (num1, num2)
